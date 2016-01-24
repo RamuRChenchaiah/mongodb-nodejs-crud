2519461eed1ecd9a0c4647d95887adb0cbd1bca6
Ramu Chenchaiah <rchencha@cisco.com>
2016-01-24 20:51:02 +0530

    Update the basic CRUD details

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CRUD' });
 });
+
+
+var users = require('../src/resources/users_operations.js')
+/* Below CRUD details are self explanatory, I hope */
+router.get('/users/all', users.getAllUserDetails);
+router.get('/users/:userId', users.getOneUserDetails);
+
+router.post('/users', users.createUserDetails);
+router.put('/users/:userId', users.updateUserDetails);
+router.delete('/users/:userId', users.removeUserDetails);
 
 module.exports = router;
---
+++ src/resources/users_operations.js
@@ -0,0 +1,31 @@
+
+var getAllUserDetails = function(req, res){
+	
+}
+
+
+var getOneUserDetails = function(req, res){
+	
+}
+
+
+var createUserDetails = function(req, res){
+	
+}
+
+
+var updateUserDetails = function(req, res){
+	
+}
+
+
+var removeUserDetails = function(req, res){
+	
+}
+
+exports.getAllUserDetails = getAllUserDetails
+exports.getOneUserDetails = getOneUserDetails
+exports.createUserDetails = createUserDetails
+exports.updateUserDetails = updateUserDetails
+exports.removeUserDetails = removeUserDetails
+

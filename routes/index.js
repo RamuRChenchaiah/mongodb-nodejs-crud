c251e1504934fb43f4d90b93e65a9ceaebbc4f6f
Ramu Chenchaiah <rchencha@cisco.com>
2016-01-25 05:56:08 +0530

    Aligned routes with respective resource

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CRUD' });
 });
 
-
-var users = require('../src/resources/users_operations.js')
-/* Below CRUD details are self explanatory, I hope */
-router.get('/users/all', users.getAllUserDetails);
-router.get('/users/:userId', users.getOneUserDetails);
-
-router.post('/users', users.createUserDetails);
-router.put('/users/:userId', users.updateUserDetails);
-router.delete('/users/:userId', users.removeUserDetails);
-
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+var users = require('../src/resources/users_operations.js')
+/* Below CRUD details are self explanatory, I hope */
+router.get('/users/all', users.getAllUserDetails);
+router.get('/users/:userId', users.getOneUserDetails);
+
+router.post('/users', users.createUserDetails);
+router.put('/users/:userId', users.updateUserDetails);
+router.delete('/users/:userId', users.removeUserDetails);
+
+
 module.exports = router;
